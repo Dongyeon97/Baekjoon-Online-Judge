d6145d221ceaf4b5912d9281dfac5a738cc3d117
Dongyeon97 <kids1677@naver.com>
2023-07-04 21:06:31 +0900

    Init : Create 11653번 소인수분해

---
+++ 01. 단계별로 풀어보기/09. 약수, 배수와 소수/11653번 소인수분해.cpp
@@ -0,0 +1,34 @@
+﻿/*
+* @title    : 11653번 소인수분해
+* @content  : 일반 수학1
+* @author   : 박동연
+* @date     : 230704
+* @time     : 0ms
+* @memory   : 2020KB
+* @state    : 완료
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N = 0;
+
+	cin >> N;
+
+	if (1 == N)
+		return 0;
+
+	for (int i = 2; i <= N; ++i)
+	{
+		while(0 == N % i)
+		{
+			cout << i << "\n";
+			N /= i;
+		}
+	}
+
+	return 0;
+}
