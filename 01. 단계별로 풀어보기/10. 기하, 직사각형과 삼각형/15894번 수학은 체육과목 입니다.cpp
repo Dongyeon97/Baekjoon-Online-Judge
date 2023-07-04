78922e661b5c786d44c83426bb3ec59ac8d3175a
Dongyeon97 <kids1677@naver.com>
2023-07-05 04:22:08 +0900

    Init : Create 15894번 수학은 체육과목 입니다

---
+++ 01. 단계별로 풀어보기/10. 기하, 직사각형과 삼각형/15894번 수학은 체육과목 입니다.cpp
@@ -0,0 +1,28 @@
+﻿/*
+* @title    : 15894번 수학은 체육과목 입니다
+* @content  : 기하 : 직사각형과 삼각형
+* @author   : 박동연
+* @date     : 230705
+* @time     : 0ms
+* @memory   : 2020KB
+* @state    : 완료
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n = 0;
+
+	unsigned long long result = 0;
+
+	cin >> n;
+
+	result = (unsigned long long)4 * n;
+
+	cout << result;
+
+	return 0;
+}
