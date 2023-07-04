bbbc6fee808f231c3e47403250dd1a7d9bbd36b8
Dongyeon97 <kids1677@naver.com>
2023-07-04 19:45:33 +0900

    Init : Create 1978번 소수 찾기

---
+++ 01. 단계별로 풀어보기/09. 약수, 배수와 소수/1978번 소수 찾기.cpp
@@ -0,0 +1,49 @@
+﻿/*
+* @title    : 1978번 소수 찾기
+* @content  : 일반 수학1
+* @author   : 박동연
+* @date     : 230704
+* @time     : 0ms
+* @memory   : 2020KB
+* @state    : 완료
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int N = 0;	// 주어지는 숫자의 개수
+	int iTemp = 0;
+	int iCount = 0;
+	int iTotalCount = 0;
+	vector<int> vec;
+
+	cin >> N;
+
+	while (N--)
+	{
+		cin >> iTemp;
+		vec.push_back(iTemp);
+	}
+
+	vector<int>::iterator iter = vec.begin();
+
+	for (iter; iter != vec.end(); ++iter)
+	{
+		iCount = 0;
+		for (int i = (*iter); i > 0; --i)
+		{
+			if (0 == (*iter) % i)
+				++iCount;
+		}
+		if (2 == iCount)
+			++iTotalCount;
+	}
+
+	cout << iTotalCount;
+
+	return 0;
+}
